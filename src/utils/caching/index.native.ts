f1a8f44f993895a2894a818d773e165f4570494e
a.sheykholeslami <a.sheykholeslami@kian.digital>
2023-11-28 16:22:46 +0330

    ADD caching functionality for react native

--- src/utils/caching/index.native.ts
+++ src/utils/caching/index.native.ts
@@ -1,23 +1,69 @@
-import { CacheItem, Caches } from "../../types/caches";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Caches } from "../../types/caches";
+import { CacheData } from "../../types/useApi";
 
-export const addNewCache = async (cacheKey: string, data: any) => {
-  const stringCaches = await AsyncStorage.getItem("caches");
-  let caches: Caches = [];
-  caches = JSON.parse(stringCaches || "");
-  if (caches.findIndex((item) => item.key === cacheKey) === -1) {
-    caches = [...caches, { key: cacheKey, value: data }];
+const getLocalStorageItem = async (key: string) => {
+  try {
+    const stringCaches = await AsyncStorage.getItem(key);
+    return JSON.parse(stringCaches || "[]");
+  } catch (error) {
+    console.error("Error retrieving from localStorage:", error);
+    return [];
   }
-  localStorage.setItem("caches", JSON.stringify(caches));
+};
+
+const setLocalStorageItem = async (key: string, value: Caches) => {
+  try {
+    await AsyncStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.error("Error storing in localStorage:", error);
+  }
+};
+
+export const addNewCache = async (cacheData: CacheData, data: any) => {
+  const caches = await getLocalStorageItem("caches");
+  const newCache = {
+    key: cacheData.key,
+    value: data,
+    timeout: cacheData.timeout,
+    fetchTime: new Date(),
+  };
+  await setLocalStorageItem("caches", [...caches, newCache]);
+};
+
+export const isCacheExpired = async (cacheKey: string) => {
+  const cacheItem = await getCacheItem(cacheKey);
+  if (cacheItem) {
+    const currentDate = new Date();
+    const fetchTime = new Date(cacheItem?.fetchTime);
+    const timeDifferenceInSeconds =
+      (currentDate.getTime() - fetchTime.getTime()) / 1000;
+    return timeDifferenceInSeconds >= cacheItem.timeout;
+  }
+  return false;
+};
+
+export const getCacheItem = async (cacheKey: string) => {
+  const caches = await getLocalStorageItem("caches");
+  return caches.find((item) => item.key === cacheKey);
+};
+
+export const isCacheExists = async (cacheKey: string) => {
+  const caches = await getLocalStorageItem("caches");
+  return caches.findIndex((item) => item.key === cacheKey) === -1;
 };
 
 export const getCacheDataWithCacheKey = async (cacheKey: string) => {
-  const stringCaches = await AsyncStorage.getItem("caches");
-  let data: CacheItem | undefined = undefined;
-  if (stringCaches) {
-    const caches: Caches = JSON.parse(stringCaches);
-    data = caches.find((item) => item.key === cacheKey);
-    console.log(caches);
+  try {
+    const caches = await getLocalStorageItem("caches");
+    const data = caches.find((item) => item.key === cacheKey);
+    if (await isCacheExpired(cacheKey)) {
+      const updatedCaches = caches.filter((item) => item.key !== cacheKey);
+      await setLocalStorageItem("caches", updatedCaches);
+      return undefined;
+    }
+    return data;
+  } catch (error) {
+    console.error("Error getting cache data:", error);
   }
-  return data;
 };
